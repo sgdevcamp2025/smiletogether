bcbb0b60a92f47fe197af41bcdc668a60ed18de7
pigpgw <ceh20002@naver.com>
2025-01-28 17:30:05 +0900

    feat: Axios, React Query, Zustand 설치 및 초기 설정을 한다

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = path.resolve();
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env': {},
+  },
+});
