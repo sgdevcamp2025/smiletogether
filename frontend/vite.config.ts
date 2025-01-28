d85d40f0a73a96358dc6830b1b8e368aa9c32cf0
pigpgw <ceh20002@naver.com>
2025-01-28 21:28:34 +0900

    fix: Tailwind CSS 설정 및 shadcn 초기화 작업

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
+import tailwindcss from 'tailwindcss';
 
-const __dirname = path.resolve();
-// https://vitejs.dev/config/
+// __dirname 대체
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  define: {
-    'process.env': {},
   },
 });
