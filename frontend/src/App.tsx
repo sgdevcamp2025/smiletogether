a2b6092abcffe43c1a8424ce22e45478294c5d52
pigpgw <ceh20002@naver.com>
2025-01-28 19:08:51 +0900

    refactor/#1 : 프로젝트 구조 및 파일 정리
    body
    - 불필요한 파일 제거 (App.css, react.svg 등)
    - App.tsx 및 main.tsx 파일 수정
    - 라우터 설정 추가 (src/routers/index.tsx)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
-import './App.css';
+import { router } from './routers';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({});
 
 function App() {
-  return <div className="text-red-500">홈 입니다. </div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './index.css'; // TailwindCSS 및 기본 스타일
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
