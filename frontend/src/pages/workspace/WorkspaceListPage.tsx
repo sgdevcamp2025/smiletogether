18bb85e3df074e95b9de6f9ef4a4bf2f8286e329
pigpgw <ceh20002@naver.com>
2025-01-29 22:15:46 +0900

    feat/#4 : 워크스페이스 리스트 페이지를 추가한다

---
+++ frontend/src/pages/workspace/WorkspaceListPage.tsx
@@ -0,0 +1,3 @@
+export const WorkspaceListPage = () => {
+  return <div>워크스페이스 리스트 페이지입니다.</div>;
+};
--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -1,8 +1,13 @@
+import { WorkspaceListPage } from '@/pages/workspace/WorkspaceListPage';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <div className="text-red-400">홈 입니다</div>,
+  },
+  {
+    path: '/workspaces',
+    element: <WorkspaceListPage />,
   },
 ]);
