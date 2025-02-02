373ba3821a5f0746f82663c795c993869595b25a
Eunjin Cho <whdmswls9207@gmail.com>
2025-02-03 04:27:02 +0900

    feat/#6 : 로그인 폼 분리

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { LoginForm } from '@/components/login/LoginForm';
+import { FormEvent, useState } from 'react';
 
 const LoginPage = () => {
+  const [isEmailSent, setIsEmailSent] = useState<boolean>(false);
+
+  const handleEmailSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //이메일 전송
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email') as string;
+    console.log(email);
+    setIsEmailSent(true);
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white p-4">
       <div className="text-2xl font-bold text-zinc-950 mb-8">smileTogether</div>
@@ -14,16 +25,7 @@ const LoginPage = () => {
             직장에서 사용하는 이메일 주소로 로그인하는 걸 추천드려요.
           </p>
         </div>
-        <div className="space-y-4">
-          <Input
-            type="email"
-            placeholder="name@work-email.com"
-            className="w-full px-3 py-2 border rounded-md"
-          />
-          <Button className="w-full bg-amber-300 hover:bg-rose-200 text-white py-2 rounded-md">
-            계속
-          </Button>
-        </div>
+        <LoginForm onSubmit={handleEmailSubmit} />
       </div>
     </div>
   );
