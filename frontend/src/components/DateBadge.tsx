a54906e2486817b932dbe5539f7a8b36dab4d9f3
yeona <aya813@naver.com>
2025-02-04 23:41:12 +0900

    refactor/#5 : formatDate 함수 분리

--- frontend/src/components/DateBadge.tsx
+++ frontend/src/components/DateBadge.tsx
@@ -1,22 +1,4 @@
-const formatDate = (isoString: string): string => {
-  const date = new Date(isoString);
-
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  const weekdays = [
-    '일요일',
-    '월요일',
-    '화요일',
-    '수요일',
-    '목요일',
-    '금요일',
-    '토요일',
-  ];
-  const weekday = weekdays[date.getDay()];
-
-  return `${month}월 ${day}일 ${weekday}`;
-};
+import { formatDate } from '@/lib/date';
 
 interface DateBadgeProps {
   date: string;
