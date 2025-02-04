1d6b07e845daac8af58a9e66d932121c4f667d30
yeona <aya813@naver.com>
2025-02-04 23:40:27 +0900

    feat/#5 : Messag 컴포넌트 UI 구현

---
+++ frontend/src/components/common/Message.tsx
@@ -0,0 +1,42 @@
+import { formatTime } from '@/lib/date';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import UserHoverCard from './UserHoverCard';
+
+interface User {
+  userId: string;
+  username: string;
+  displayName: string;
+  profileImage: string;
+}
+
+interface MessageProps {
+  user: User;
+  content: string;
+  createdAt: string;
+}
+
+const Message = ({ user, content, createdAt }: MessageProps) => {
+  return (
+    <div className="flex gap-2">
+      <Avatar className="rounded-lg w-12 h-12">
+        <AvatarImage src={user.profileImage} alt="유저의 프로필이미지" />
+        <AvatarFallback>{user.username}</AvatarFallback>
+      </Avatar>
+      <div>
+        <div className="flex gap-2 items-center">
+          <UserHoverCard
+            username={user.username}
+            displayName={user.displayName}
+            profileImage="https://github.com/shadcn.png"
+            isActive={true}
+            isMessage={true}
+          />
+          <p className="text-zinc-400 text-sm">{formatTime(createdAt)}</p>
+        </div>
+        <p className="text-base">{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
