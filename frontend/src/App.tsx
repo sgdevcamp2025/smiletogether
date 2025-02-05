c56292fa7497fe71d9de2284e1bb9518845d0eee
Eunjin Cho <whdmswls9207@gmail.com>
2025-02-05 16:28:31 +0900

    fix/#6 : 안쓰는 import 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { router } from './routers';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import LoginPage from './pages/login/LoginPage';
 
 const queryClient = new QueryClient({});
 
