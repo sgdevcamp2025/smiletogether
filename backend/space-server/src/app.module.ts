54d17de07a0a63a4210e5027d3b86038eecff28a
Eunjin Cho <whdmswls9207@gmail.com>
2025-02-07 04:39:28 +0900

    feat/#27 : prisma 설치

--- backend/space-server/src/app.module.ts
+++ backend/space-server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkspaceModule } from './workspace/workspace.module';
 
 @Module({
-  imports: [WorkspaceModule],
+  imports: [ConfigModule.forRoot(), WorkspaceModule],
   controllers: [AppController],
   providers: [AppService],
 })
