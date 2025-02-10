d710918a224af3cbb36a9f9f54b88fd398c0d708
imjanghyeok <qkdrhkgn23@naver.com>
2025-02-11 07:44:23 +0900

    "feat/#33: 메시지를 생산하여 kafka로 전달하는 producer 수정
    - 기존에 있던 토픽이 수정되어 토픽을 수정했습니다.
    - 단순 메시지가 아닌 객체를 보내기 위해서 json 문자열로 변환하는 과정을 추가했습니다.
    - 변환 객체 dto가 수정되어 변경했습니다.
    "

--- backend/chat-server/src/main/java/com/smiletogether/chatserver/service/MessageProducer.java
+++ backend/chat-server/src/main/java/com/smiletogether/chatserver/service/MessageProducer.java
@@ -1,7 +1,7 @@
 package com.smiletogether.chatserver.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.smiletogether.chatserver.service.dto.MessageDto;
+import com.smiletogether.chatserver.service.dto.ChannelChatDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -13,14 +13,14 @@ import org.springframework.stereotype.Service;
 public class MessageProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate; // JSON을 문자열로 전송
-    private final ObjectMapper objectMapper = new ObjectMapper(); // JSON 변환 객체
-    private static final String EXISTING_TOPIC = "test-topic";
+    private final ObjectMapper objectMapper; // 주입받도록 변경
+    private static final String TOPIC = "chat-topic";
 
-    public void sendMessage(MessageDto message) {
+    public void sendMessage(ChannelChatDto channelChatDto) {
         try {
-            String jsonMessage = objectMapper.writeValueAsString(message); // 객체 -> JSON 변환
+            String jsonMessage = objectMapper.writeValueAsString(channelChatDto); // 객체 -> JSON 변환
             log.info("Sending message to Kafka as JSON: {}", jsonMessage);
-            kafkaTemplate.send(EXISTING_TOPIC, jsonMessage); // JSON 문자열로 Kafka에 전송
+            kafkaTemplate.send(TOPIC, jsonMessage); // JSON 문자열로 Kafka에 전송
         } catch (Exception e) {
             log.error("Failed to serialize message", e);
         }
