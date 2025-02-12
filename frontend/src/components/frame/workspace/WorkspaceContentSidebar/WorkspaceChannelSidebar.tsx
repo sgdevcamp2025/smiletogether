0600ba19ba9adfc7c9f88813af8508417f1f98cd
pigpgw <ceh20002@naver.com>
2025-02-13 01:58:57 +0900

    remove/#52 : 불필요한 코드를 삭제한다

--- frontend/src/components/frame/workspace/WorkspaceContentSidebar/WorkspaceChannelSidebar.tsx
+++ frontend/src/components/frame/workspace/WorkspaceContentSidebar/WorkspaceChannelSidebar.tsx
@@ -1,27 +1,23 @@
-import useWorkspaceChannelListQuery from '@/hooks/channel/useWorkspaceChannelListQuery';
 import { useEffect } from 'react';
 import { useParams } from 'react-router';
-import useUserWorkspacesQuery from '@/hooks/workspace/useUserWorkspacesQuery';
 import useUserWorkspaceQuery from '@/hooks/workspace/useUserWorkspaceQuery';
+import useWorkspaceChannelListQuery from '@/hooks/channel/useWorkspaceChannelListQuery';
 
 const WorkspaceChannelSidebar = () => {
   const { workspaceID } = useParams();
-  const { data, isLoading, isError } = useUserWorkspaceQuery(workspaceID!);
+  const {
+    data: workspacesInfo,
+    isLoading: isWorkspaceLoading,
+    isError: isWorkspaceError,
+  } = useUserWorkspaceQuery(workspaceID!);
 
   const {
     data: channelList,
     isLoading: isChannelLoading,
     isError: isChannelError,
   } = useWorkspaceChannelListQuery(workspaceID!);
-
-  const {
-    data: workspaceInfo,
-    isLoading: isWorkspaceLoading,
-    isError: isWorkspaceError,
-  } = useUserWorkspacesQuery();
-
   useEffect(() => {
-    console.log('params', workspaceID, 'data', data);
+    console.log('params', workspaceID, 'workspacesInfo', workspacesInfo);
   }, []);
 
   if (isChannelLoading || isWorkspaceLoading) return <p>로딩 중입니다!</p>;
@@ -29,7 +25,9 @@ const WorkspaceChannelSidebar = () => {
 
   return (
     <div className="text-wrap">
-      <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight"></h3>
+      <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+        {workspacesInfo?.name}
+      </h3>
     </div>
   );
 };
