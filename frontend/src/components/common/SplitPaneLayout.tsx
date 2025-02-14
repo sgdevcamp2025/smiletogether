3b5ad5154d7a9ee7d2ef8f98e68122de69ab38c0
pigpgw <ceh20002@naver.com>
2025-02-15 01:48:19 +0900

    style/#52 : SplitPaneLayout 기본 너비를 수정한다

--- frontend/src/components/common/SplitPaneLayout.tsx
+++ frontend/src/components/common/SplitPaneLayout.tsx
@@ -13,9 +13,9 @@ interface SplitPaneLayoutProps {
 const SplitPaneLayout = ({ children1, children2 }: SplitPaneLayoutProps) => {
   return (
     <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel defaultSize={30}>{children1}</ResizablePanel>
+      <ResizablePanel defaultSize={20}>{children1}</ResizablePanel>
       <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={70}>{children2}</ResizablePanel>
+      <ResizablePanel defaultSize={80}>{children2}</ResizablePanel>
     </ResizablePanelGroup>
   );
 };
