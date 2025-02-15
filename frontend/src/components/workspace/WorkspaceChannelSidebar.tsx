6de96614a9f0ca0e5aec89045394a6a14456bc94
pigpgw <ceh20002@naver.com>
2025-02-15 15:47:25 +0900

    style/#52 : 스타일을 수정한다

--- frontend/src/components/workspace/WorkspaceChannelSidebar.tsx
+++ frontend/src/components/workspace/WorkspaceChannelSidebar.tsx
@@ -31,7 +31,7 @@ const WorkspaceChannelSidebar = () => {
     return <p>에러가 발생했습니다!</p>;
 
   return (
-    <div className=" min-w-16 bg-yellow-200 text-white flex py-2 flex-col items-center gap-2 text-wrap h-screen">
+    <div className=" min-w-16 bg-yellow-200 text-white flex py-2 flex-col gap-2 text-wrap h-screen">
       <h2 className="mt-3 px-4 scroll-m-20 text-2xl font-semibold tracking-tight text-white">
         {workspacesInfo?.name}
       </h2>
