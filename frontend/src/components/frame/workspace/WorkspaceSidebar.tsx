30bf2fea7eb0b37848ed0f975a0fa8285ebf13f2
pigpgw <ceh20002@naver.com>
2025-02-16 00:12:15 +0900

    refactor/52 : 더미데이터를 수정한다

--- frontend/src/components/frame/workspace/WorkspaceSidebar.tsx
+++ frontend/src/components/frame/workspace/WorkspaceSidebar.tsx
@@ -1,9 +1,11 @@
 import WorkspaceIconButton from '@/components/workspace/WorkspaceIconButton';
 import useUserWorkspacesQuery from '@/hooks/workspace/useUserWorkspacesQuery';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
 const WorkspaceSideBar = () => {
   const { data, isLoading, isError } = useUserWorkspacesQuery();
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log('WorkspaceSideBar', data);
@@ -12,6 +14,10 @@ const WorkspaceSideBar = () => {
   if (isLoading) return <p>로딩중</p>;
   if (isError) return <p>에러</p>;
 
+  const navigateToWorkspace = (workspaceId: string) => {
+    navigate(`/workspace/${workspaceId}`);
+  };
+
   return (
     <div className=" min-w-16 bg-yellow-400 text-white flex py-2 flex-col items-center gap-2">
       <div className="flex flex-col items-center justify-center">
@@ -19,8 +25,8 @@ const WorkspaceSideBar = () => {
           data.workspaces.map((item, index) => {
             return (
               <WorkspaceIconButton
-                key={index}
                 className="bg-gray-400 rounded-xl"
+                onClick={() => navigateToWorkspace(item.workspace_id)}
               >
                 {item.name ? item.name.slice(0, 2) : 'W'}
               </WorkspaceIconButton>
