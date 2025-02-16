34252ee6b230cf4fcb0f88198d938c82802cbb67
pigpgw <ceh20002@naver.com>
2025-02-16 13:16:58 +0900

    style/#52 : WorkspaceUserInviteModal 스타일을 조정한다

--- frontend/src/components/workspace/Modal/WorkspaceUserInviteModal.tsx
+++ frontend/src/components/workspace/Modal/WorkspaceUserInviteModal.tsx
@@ -17,37 +17,37 @@ const WorkspaceUserInviteModal = ({
 
   return (
     <ModalPortal>
-      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[500px] bg-white shadow-lg rounded-lg p-6">
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 max-w-[800px] min-w-[400px] bg-white shadow-lg rounded-lg p-6">
         <div className="flex justify-between items-center border-b pb-3">
           <h1 className="text-lg font-semibold">{title}(으)로 사용자 초대</h1>
           <Button
             onClick={closeModal}
-            className="text-gray-500 hover:text-gray-700"
+            className="text-gray-500 hover:text-gray-700 bg-transparent shadow-none"
           >
             x
           </Button>
         </div>
 
-        <div className="mt-4">
+        <div className="mt-4 w-full">
           <span className="text-gray-700 text-sm">초대 받을 사람 :</span>
-          <div className="mt-2 border rounded-md p-2">
+          <div className="mt-2 rounded-md">
             <EmailTagInput
               emails={emails}
               setEmails={setEmails}
               setIsValidEmail={setIsValid}
             />
           </div>
-          <Button className="mt-3 text-blue-600 hover:underline text-sm">
-            ✨ 초대 사용자 지정
-          </Button>
         </div>
 
+        <Button className="mt-3 text-blue-600 hover:underline text-sm w-full py-6 bg-gray-100">
+          ✨ 초대 사용자 지정
+        </Button>
         <div className="mt-6 flex justify-between border-t pt-4">
-          <Button className="text-blue-500 hover:underline text-sm">
+          <Button className="text-blue-500 hover:underline text-sm bg-transparent shadow-none ">
             🔗 초대 링크 복사
           </Button>
-          <Button className="text-gray-500 hover:text-gray-700 text-sm">
-            🔧 링크 설정 편집
+          <Button className="text-gray-500 hover:text-gray-700 text-sm font-black bg-gray-100 shadow-none px-6">
+            보내기
           </Button>
         </div>
       </div>
