c14d032f82c21c1bd3684243fcdf68c6fac37ff0
imjanghyeok <qkdrhkgn23@naver.com>
2025-02-26 10:09:30 +0900

    refactor/#31: 서버 시간 디폴트값 지정
    - 디폴트 서버 시간을 아시아/서울로 고정했습니다.

--- backend/chat-server/src/main/java/com/smiletogether/chatserver/ChatServerApplication.java
+++ backend/chat-server/src/main/java/com/smiletogether/chatserver/ChatServerApplication.java
@@ -1,5 +1,6 @@
 package com.smiletogether.chatserver;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatServerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ChatServerApplication.class, args);
 	}
 
