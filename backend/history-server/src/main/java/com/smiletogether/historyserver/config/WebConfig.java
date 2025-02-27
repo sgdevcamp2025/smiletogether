73209214b7876bc5bc878d6e914784216e481237
imjanghyeok <66295199+imjanghyeok@users.noreply.github.com>
2025-02-28 00:24:41 +0900

    fix: CORS 설정 (#119)

--- backend/history-server/src/main/java/com/smiletogether/historyserver/config/WebConfig.java
+++ backend/history-server/src/main/java/com/smiletogether/historyserver/config/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("Authorization", "Content-Type")
-                .allowCredentials(true);
+                .allowedHeaders("Authorization", "Content-Type");
     }
 }
