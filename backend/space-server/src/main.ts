32f3cf8f6ad6bfc6819e8d6b1703940aab587c09
Eunjin Cho <whdmswls9207@gmail.com>
2025-02-27 14:09:10 +0900

    feat/#109 : origin 허용

--- backend/space-server/src/main.ts
+++ backend/space-server/src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://smiletogether.store',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 8090);
 }
 bootstrap();
