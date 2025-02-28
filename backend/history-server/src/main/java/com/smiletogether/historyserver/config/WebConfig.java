a4d737dd3c7e15c59d226ed9f15729b2ef46769f
imjanghyeok <66295199+imjanghyeok@users.noreply.github.com>
2025-02-28 16:05:27 +0900

    [BE] fix: 채팅 리스트 정렬 수정 (#124)
    * fix-#121: 데이터 정렬 수정
    
    * fix-#121: 데이터 정렬 테스트를 위한 시간 수정
    
    * fix-#121: post man 테스트 관련 cors 설정

--- backend/history-server/src/main/java/com/smiletogether/historyserver/config/WebConfig.java
+++ backend/history-server/src/main/java/com/smiletogether/historyserver/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("Authorization", "Content-Type");
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(false);
     }
 }
--- backend/history-server/src/main/java/com/smiletogether/historyserver/service/ChannelMessageService.java
+++ backend/history-server/src/main/java/com/smiletogether/historyserver/service/ChannelMessageService.java
@@ -48,14 +48,14 @@ public class ChannelMessageService {
     }
 
     public ChannelMessages getChannelMessages(ChannelMessagesRequest channelMessagesRequest, String workspaceId,
-            String channelId) {
+                                              String channelId) {
         log.info("📌 요청받은 afterTime: {}", channelMessagesRequest.lastTimeStamp());
 
         LocalDateTime afterTime = channelMessagesRequest.lastTimeStamp();
         log.info("📌 변환된 afterTime (LocalDateTime): {}", afterTime);
 
         List<ChannelMessageDocument> channelMessageDocuments = channelMessageRepository.findByWorkspaceIdAndChannelIdAndIsDeletedAndCreatedAtBefore(
-                workspaceId, channelId, false, afterTime, PageRequest.of(0, 20, Sort.by(Direction.ASC, "createdAt"))
+                workspaceId, channelId, false, afterTime, PageRequest.of(0, 20, Sort.by(Direction.DESC, "createdAt"))
         );
 
         log.info("📌 검색된 메시지 개수: {}", channelMessageDocuments.size());
@@ -73,7 +73,13 @@ public class ChannelMessageService {
             groupedMessages.computeIfAbsent(dateKey, k -> new ArrayList<>()).add(messageResponse);
         }
 
-        return new ChannelMessages(channelId, groupedMessages);
+        // 날짜 순서대로 그룹화된 메시지를 내림차순으로 정렬하여 가장 오래된 날짜부터 보이도록 설정
+        Map<String, List<ChannelMessageResponse>> reversedGroupedMessages = new LinkedHashMap<>();
+        groupedMessages.entrySet().stream()
+                .sorted(Map.Entry.<String, List<ChannelMessageResponse>>comparingByKey()) // 날짜 오름차순으로 정렬
+                .forEachOrdered(entry -> reversedGroupedMessages.put(entry.getKey(), entry.getValue()));
+
+        return new ChannelMessages(channelId, reversedGroupedMessages);
     }
 
     public void saveMessage(ChannelMessageSaveRequest channelMessage) {
