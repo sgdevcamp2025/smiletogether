ff16f5c76564348e7c0b2e5adc91ae36a7a0e5da
Eunjin Cho <whdmswls9207@gmail.com>
2025-03-03 04:38:51 +0900

    feat/#125 : generate invite code

--- backend/space-server/src/invite/invite.controller.ts
+++ backend/space-server/src/invite/invite.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { InviteService } from './invite.service';
 
-@Controller('invite')
+@Controller('api/invite')
 export class InviteController {
   constructor(private readonly inviteService: InviteService) {}
+
+  @Post('generatelink')
+  async createInviteLink(
+    @Body() body: { domain: string; workspaceId: string },
+  ) {
+    const inviteLink = await this.inviteService.generateInviteLink(
+      body.domain,
+      body.workspaceId,
+    );
+    return { inviteLink };
+  }
 }
--- backend/space-server/src/invite/invite.service.ts
+++ backend/space-server/src/invite/invite.service.ts
@@ -1,12 +1,32 @@
 import { RedisService } from '@liaoliaots/nestjs-redis';
 import { Injectable } from '@nestjs/common';
 import { Redis } from 'ioredis';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class InviteService {
   private readonly redis: Redis | null;
+  private readonly INVITE_CODE_EXPIRATION = 60 * 60; // 1시간
 
   constructor(private readonly redisService: RedisService) {
     this.redis = this.redisService.getOrThrow();
+  }
+
+  async generateInviteLink(
+    domain: string,
+    workspaceId: string,
+  ): Promise<string> {
+    const inviteCode = uuidv4();
+    const inviteKey = `invite_link:${inviteCode}`;
+    const inviteUrl = `${domain}/invite/link/${inviteCode}`;
+
+    await this.redis.set(
+      inviteKey,
+      workspaceId,
+      'EX',
+      this.INVITE_CODE_EXPIRATION,
+    );
+
+    return inviteUrl;
   }
 }
