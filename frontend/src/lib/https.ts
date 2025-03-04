91525b0817a1ad1a5e9f0a766975cd5569d519f2
pigpgw <ceh20002@naver.com>
2025-03-04 23:02:02 +0900

    fix/#134 : interceptor 예외처리 부족으로 인한 문제를 해결한다

--- frontend/src/lib/https.ts
+++ frontend/src/lib/https.ts
@@ -26,12 +26,15 @@ https.interceptors.request.use(
 
 https.interceptors.response.use(
   response => {
-    const accessToken = response.headers.authorization.split(' ')[1];
-    localStorage.setItem('access-token', accessToken);
-    localStorage.setItem(
-      'refresh-token',
-      '        eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcklkIjoiMDNjNmIwODMtZThkNi00ODhjLWFhODMtMmEwMWIzZjM5ZDAwIiwiaWF0IjoxNTE2MjM5MDIyfQ.iVTdh4kkGh6f6gEZLf9MJPwkjusaXf58z_Tc4ncummw'
-    );
+    const authorization = response.headers.Authorization;
+    if (authorization) {
+      const accessToken = response.headers.authorization.split(' ')[1];
+      localStorage.setItem('access-token', accessToken);
+      localStorage.setItem(
+        'refresh-token',
+        '        eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcklkIjoiMDNjNmIwODMtZThkNi00ODhjLWFhODMtMmEwMWIzZjM5ZDAwIiwiaWF0IjoxNTE2MjM5MDIyfQ.iVTdh4kkGh6f6gEZLf9MJPwkjusaXf58z_Tc4ncummw'
+      );
+    }
     return response;
   },
   error => {
