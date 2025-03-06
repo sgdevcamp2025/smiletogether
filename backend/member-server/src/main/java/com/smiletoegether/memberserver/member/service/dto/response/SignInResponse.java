ec298fc38db5eac6cb04debee6cbadbbd79b2ef5
imjanghyeok <qkdrhkgn23@naver.com>
2025-03-07 00:29:54 +0900

    refactor/#158: 로직 수정에 따른 dto 수정

--- backend/member-server/src/main/java/com/smiletoegether/memberserver/member/service/dto/response/SignInResponse.java
+++ backend/member-server/src/main/java/com/smiletoegether/memberserver/member/service/dto/response/SignInResponse.java
@@ -1,10 +1,13 @@
 package com.smiletoegether.memberserver.member.service.dto.response;
 
 import com.smiletoegether.memberserver.common.dto.CommonCodeResponse;
+import com.smiletoegether.memberserver.member.domain.Member;
 
 public record SignInResponse(
         String accessToken,
-        CommonCodeResponse response
+        CommonCodeResponse response,
+        boolean isMember,
+        Member member
 ) {
 
 }
