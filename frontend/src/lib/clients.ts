8453baac3c5d29d9f0ea186afc09882dd1c3b696
pigpgw <ceh20002@naver.com>
2025-03-24 12:28:06 +0900

    refactor/#201 : 모킹 API url 환경변수로 분리

--- frontend/src/lib/clients.ts
+++ frontend/src/lib/clients.ts
@@ -4,4 +4,6 @@ export const userApi = createApi(import.meta.env.VITE_BASE_USER_API_URL);
 export const chatApi = createApi(import.meta.env.VITE_BASE_CHAT_API_URL);
 export const spaceApi = createApi(import.meta.env.VITE_BASE_SPACE_API_URL);
 export const authApi = createApi(import.meta.env.VITE_BASE_AUTH_API_URL);
-export const directMessageApi = createApi('/api/dms');
+export const directMessageApi = createApi(
+  import.meta.env.VITE_BASE_AUTH_API_URL
+);
