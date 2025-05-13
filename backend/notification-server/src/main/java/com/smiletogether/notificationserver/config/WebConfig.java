560b3cee278f68f833577cecd406d21872a8d96f
imjanghyeok <qkdrhkgn23@naver.com>
2025-05-14 08:54:54 +0900

    feat/#227: webPush test를 위한 보안 무시 설정, RestTemplate 설정 및 웹 cors 설정

---
+++ backend/notification-server/src/main/java/com/smiletogether/notificationserver/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.smiletogether.notificationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 경로 허용
+                        .allowedOrigins("http://localhost:3000", "http://localhost:8090", "http://localhost:8091") // React 앱 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
